4da37f5b856c783b00e7be8cac43ef1044e315ca
Roxana Radutiu <roxiibiia97@gmail.com>
2025-11-06 23:34:51 +0200

    feat: replace page refresh with Roll Dice button

--- index.js
+++ index.js
@@ -8,35 +8,36 @@ var arrayImages = [
     './images/dice6.png',
 ];
 
-// Generate random numbers from 0 to 5 for each player
-var randomNumber1 = Math.floor(Math.random() * arrayImages.length);
-var randomNumber2 = Math.floor(Math.random() * arrayImages.length);
+function rollDice() {
+    // Generate random numbers from 0 to 5 for each player
+    var randomNumber1 = Math.floor(Math.random() * arrayImages.length);
+    var randomNumber2 = Math.floor(Math.random() * arrayImages.length);
 
-// Select the HTML elements for images, title and subtitle to manipulate them
-var imag1 = document.querySelector(".img1");
-var imag2 = document.querySelector(".img2");
-var title = document.querySelector("h1");
-var refresh = document.querySelector("p");
+    // Select the HTML elements for images, title and subtitle to manipulate them
+    var imag1 = document.querySelector(".img1");
+    var imag2 = document.querySelector(".img2");
+    var title = document.querySelector("h1");
+    var refresh = document.querySelector("p");
+    var roll_Button = document.querySelector("#roll_Button");
 
-// Select the images randomly from the array for each player
-var randomImage1 = arrayImages[randomNumber1];
-imag1.setAttribute("src", randomImage1);
-var randomImage2 = arrayImages[randomNumber2];
-imag2.setAttribute("src", randomImage2);
+    // Select the images randomly from the array for each player
+    var randomImage1 = arrayImages[randomNumber1];
+    imag1.setAttribute("src", randomImage1);
+    var randomImage2 = arrayImages[randomNumber2];
+    imag2.setAttribute("src", randomImage2);
 
-//subtitle
-refresh.innerHTML = "Refresh the page to play again!";
-
-// Conditions to determine the winner
-if(randomImage1 === randomImage2) {
-    title.innerHTML = "Draw!";
-} 
-else if(randomImage1 > randomImage2) {
-    title.innerHTML = "🚩Player 1 Wins!"   
+    // Conditions to determine the winner
+    if(randomImage1 === randomImage2) {
+        title.innerHTML = "Draw!";
+    } 
+    else if(randomImage1 > randomImage2) {
+        title.innerHTML = "🚩Player 1 Wins!"   
+    }
+    else {
+        title.innerHTML = "Player 2 Wins!🚩"
+    }
 }
-else {
-    title.innerHTML = "Player 2 Wins!🚩"
-}
-
+// When button is clicked, roll the dice
+roll_Button.addEventListener('click', rollDice);
 
 
